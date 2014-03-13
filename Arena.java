812ecf3781c22bacf61a0c27c569847da1d3bf4b
Andrew Hood <andrewhood125@gmail.com>
2014-03-12 22:16:37 -0500

    Organize Arena class.

--- Arena.java
+++ Arena.java
@@ -1,34 +1,43 @@
 /**
- * Author: Andrew Hood <andrewhood125@gmail.com>
- * Description: Store the boundaries for arena 
+ * Store the boundaries for arena 
+ * @author Andrew Hood <andrewhood125@gmail.com>
+ * @version 0.1
+ * 
  * Copyright (c) 2014 Andrew Hood. All rights reserved.
  */
+// It should implement locatable
 class Arena extends Locate
 {
-  double north, south, east, west,size;
+    /**
+     * Instance variables
+     */
+    double north, south, east, west,size;
 
-  Arena(double latitude, double longitude, double arenaSize)
-  {
-    west = latitude - arenaSize;
-    east = latitude + arenaSize;
-    north = longitude + arenaSize;
-    south = longitude - arenaSize; 
-  }
+    Arena(double latitude, double longitude, double arenaSize)
+    {
+        west = latitude - arenaSize;
+        east = latitude + arenaSize;
+        north = longitude + arenaSize;
+        south = longitude - arenaSize; 
+    }
 
-  public double getNorth()
-  {
-    return north;
-  }
-  public double getSouth()
-  {
-    return south;
-  }
-  public double getEast()
-  {
-    return east;
-  }
-  public double getWest()
-  {
-    return west;
-  }
+    public double getEast()
+    {
+        return east;
+    }
+
+    public double getNorth()
+    {
+        return north;
+    }
+
+    public double getSouth()
+    {
+        return south;
+    }
+
+    public double getWest()
+    {
+        return west;
+    }
 }
