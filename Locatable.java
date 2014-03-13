25630852551e739bde65457654e70a79e8d078f6
Andrew Hood <andrewhood125@gmail.com>
2014-03-12 22:24:04 -0500

    Change Locate abstract class to Locatable interface.

---
+++ Locatable.java
@@ -0,0 +1,11 @@
+/**
+ * Author: Andrew Hood <andrewhood125@gmail.com>
+ * Description: Store common elements that have a location.
+ * Copyright (c) 2014 Andrew Hood. All rights reserved.
+ */
+
+public interface Locatable
+{
+  public double getLatitude();
+  public double getLongitude();
+}
