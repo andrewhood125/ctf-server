8e199ad85e158ea0c6b390e161d86a80c9721a5f
Andrew Hood <andrewhood125@gmail.com>
2014-03-12 22:20:28 -0500

    Organize Base class.

--- Base.java
+++ Base.java
@@ -1,41 +1,50 @@
 /**
- * Author: Andrew Hood <andrewhood125@gmail.com>
- * Description: The location of a teams base. 
+ * The location of a teams base. 
+ * 
+ * @author Andrew Hood <andrewhood125@gmail.com>
+ * @version 0.1
+ * 
  * Copyright (c) 2014 Andrew Hood. All rights reserved.
  */
 
-class Base extends Locate
+// Shold implement locatable
+public class Base extends Locate
 {
+    /**
+     * Instanve variables
+     */
+    double west, east, north, south;
 
-  // Base range
-  double west, east, north, south;
-  Base(double latitude, double longitude, double accuracy)
-  {
-    this.latitude = latitude;
-    this.longitude = longitude;
-    west = latitude - accuracy;
-    east = latitude + accuracy;
-    north = longitude + accuracy;
-    south = longitude - accuracy;
-  } 
+    /**
+     * Constructors
+     */
+    Base(double latitude, double longitude, double accuracy)
+    {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        west = latitude - accuracy;
+        east = latitude + accuracy;
+        north = longitude + accuracy;
+        south = longitude - accuracy;
+    } 
 
-  public double getWest()
-  {
-    return west;
-  }
+    public double getEast()
+    {
+        return east;
+    }
 
-  public double getEast()
-  {
-    return east;
-  }
+    public double getNorth()
+    {
+        return north;
+    }
 
-  public double getNorth()
-  {
-    return north;
-  }
+    public double getSouth()
+    {
+        return south;
+    }
 
-  public double getSouth()
-  {
-    return south;
-  }
+    public double getWest()
+    {
+        return west;
+    }
 }
