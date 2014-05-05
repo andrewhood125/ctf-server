188e349df2ada4425722d1a76d1627d45c5bccf2
Andrew Hood <andrewhood125@gmail.com>
2014-05-04 20:30:44 -0500

    Add WebPlayerException.

---
+++ WebPlayerException.java
@@ -0,0 +1,14 @@
+
+/**
+ * Thrown when a improperly formmated Point is given.
+ * 
+ * @author Andrew Hood
+ * @version 0.1
+ */
+public class WebPlayerException extends Exception
+{
+    public WebPlayerException(String message)
+    {
+        super(message);
+    }
+}
